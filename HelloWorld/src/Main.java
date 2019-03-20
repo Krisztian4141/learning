bb8db443176e529bd6e81e1506fd98bf3f334765
arvisura87 <jakab.tamas.me@gmail.com>
2019-03-20 21:21:55 +0100

    homework 02

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world, hello Tamás!");
+        greetFunction("alma");
+        greetFunction("korte");
+        greetFunction("banan");
+    }
+
+    public static void greetFunction(String input) {
+        System.out.println(input);
     }
 }
